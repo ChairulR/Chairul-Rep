1aaae0dc0ae9c3f4625299498e141dbf85de0d05
ChairulR <148623628+ChairulR@users.noreply.github.com>
2024-09-17 22:06:17 +0700

    Update KonversiSuhu.java

--- 23100_Chairul_Praktikum_Tugas3/src/main/java/Two/KonversiSuhu.java
+++ 23100_Chairul_Praktikum_Tugas3/src/main/java/Two/KonversiSuhu.java
@@ -9,12 +9,10 @@ package Two;
  * @author mchai
  */
 class KonversiSuhu {
-    // Method untuk mengonversi dari Celcius ke Fahrenheit
     public double celciusToFahrenheit(double celcius) {
         return (celcius * 9 / 5) + 32;
     }
     
-    // Method untuk mengonversi dari Celcius ke Reamur
     public double celciusToReamur(double celcius) {
         return celcius * 4 / 5;
     }
